a89b3b09ee9c447daf3dfb70a9ea974bb9a4e082
Kevin M Granger <kmg2728@rit.edu>
2016-03-19 17:18:53 -0400

    Add level boundaries

---
+++ Assets/Scripts/LevelBoundary.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class LevelBoundary : MonoBehaviour {
+
+    public UnityEvent onPlayerLeave;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("DED");
+            onPlayerLeave.Invoke();
+        }
+        else
+        {
+            Debug.Log("Something exited, idk what tho");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerDiedResponder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDiedResponder : MonoBehaviour {
+
+    public GameObject player;
+
+    public Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        if (!player) player = GameObject.Find("Player");
+        startPosition = player.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OhFuck()
+    {
+        Debug.Log("Can you fucking not");
+        player.transform.localPosition = startPosition;
+    }
+}
