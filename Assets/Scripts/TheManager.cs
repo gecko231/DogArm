ff3ef0f237431dd1bdf57491accd964e9991849d
Kevin M Granger <kmg2728@rit.edu>
2016-03-19 23:57:10 -0400

    Add pausing and stuff.
    Note that it doesn't actually pause. We have to decide how to handle
    that first

---
+++ Assets/Scripts/TheManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState
+{
+    Playing,
+    Paused
+}
+
+public class TheManager : MonoBehaviour {
+
+    public GameState gameState;
+    public GameObject pauseMenu;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    public void Pause()
+    {
+        gameState = GameState.Paused;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        gameState = GameState.Playing;
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            switch (gameState)
+            {
+                case GameState.Playing:
+                    Pause();
+                    break;
+                case GameState.Paused:
+                    Resume();
+                    break;
+                default:
+                    break;
+            }
+    }
+}
