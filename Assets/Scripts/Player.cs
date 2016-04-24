8b7cf8031e4c283e709c641a8fd2c05442eb958f
Fatmuffin93 <dbukhari807@gmail.com>
2016-04-24 18:51:25 -0400

    Player hurt
    The player now gets hurt if he gets into contacts with the enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     bool facingRight = false;
+	bool canMove = true;
+	int timer = 30;
     private Rigidbody2D rb;
     public float speed = 15f;
     public float jumpForce = 175f;
     private float maxSpeed = 3f;
     private bool canJump = false;
 	float myWidth;
+	public float invincibleTimeAfterHurt = 2;
 	public LayerMask playerMask;
 	Transform myTrans;
     Transform playerGraphics; // reference to graphics for changing direction
@@ -47,7 +50,7 @@ public class Player : MonoBehaviour {
     void ProcessInput()
     {
         // moving right
-	    if((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && rb.velocity.x < maxSpeed)
+	    if((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && rb.velocity.x < maxSpeed && canMove)
         {
             rb.AddForce(Vector2.right * speed);
             if(!facingRight){ // change direction of sprite
@@ -55,7 +58,7 @@ public class Player : MonoBehaviour {
             }
         }
         // moving left
-        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && rb.velocity.x > -maxSpeed)
+		if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && rb.velocity.x > -maxSpeed && canMove)
         {
             rb.AddForce(-Vector2.right * speed);
             if(facingRight){ // change direction of sprite
@@ -64,7 +67,7 @@ public class Player : MonoBehaviour {
         }
         animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
         // jumping
-        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && canJump)
+		if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && canJump && canMove)
         {
             rb.AddForce(Vector2.up * jumpForce);
             canJump = false;
@@ -74,6 +77,13 @@ public class Player : MonoBehaviour {
             gameObject.transform.position = Vector2.zero;
             rb.velocity = Vector2.zero;
         }
+		if (!canMove) {
+			timer--;
+			if (timer <= 0) {
+				canMove = true;
+				timer = 30;
+			}
+		}
     }
 
 	void FixedUpdate(){
@@ -89,9 +99,42 @@ public class Player : MonoBehaviour {
 		}
 	}
 
+	void Hurt(){
+		//Change Health here
+		rb.velocity = Vector2.zero;
+		canMove = false;
+		StartCoroutine(HurtBlinker());
+		if (facingRight) { // change direction of sprite
+			rb.AddForce (-Vector2.right * 100f);
+		} else {
+			rb.AddForce (Vector2.right * 100f);
+		}
+		rb.AddForce(Vector2.up * jumpForce);
+	}
+
+	IEnumerator HurtBlinker(){
+		//Ignore Enemy Collisions
+		int enemyLayer = LayerMask.NameToLayer ("Enemy");
+		int playerLayer = LayerMask.NameToLayer ("Player");
+		Physics2D.IgnoreLayerCollision (enemyLayer, playerLayer);
+
+		//Start blinking animation
+		animator.SetLayerWeight(1,1);
+
+		//Wait
+		yield return new WaitForSeconds(invincibleTimeAfterHurt);
+
+		//Stop animation and let collisions resume
+		Physics2D.IgnoreLayerCollision (enemyLayer, playerLayer,false);
+		animator.SetLayerWeight(1,0);
+	}
+
     void OnCollisionEnter2D(Collision2D col)
     {
-    
+		Enemy enemy = col.collider.GetComponent<Enemy> ();
+		if (enemy != null) {
+			Hurt();
+		}
     }
 
     void Flip (){
