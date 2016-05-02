58e1391a5bb7d1171013bf29453d0e6fe9a6a9a0
Kevin M Granger <github@kevinmgranger.me>
2016-05-01 21:39:35 -0400

    Add an event system for game state changes. Also, start using input settings

--- Assets/Scripts/TheManager.cs
+++ Assets/Scripts/TheManager.cs
@@ -1,25 +1,77 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.Events;
 
+/// <summary>
+/// Global Game State. This is for stuff that reflects upon overall state and control
+/// of the game, not just individual components.
+/// </summary>
 public enum GameState
 {
+    /// <summary>
+    /// This should never occur from normal code usage: it represents a default value
+    /// since enums are value types, and we can't make them nullable with this version
+    /// of mono.
+    /// </summary>
+    Null,
     Playing,
     Paused,
-    Cutscene
+    Cutscene,
+    /// <summary>
+    /// Something like an in-world GUI, like the quick select menu.
+    /// </summary>
+    InGameMenu,
 }
 
-public class TheManager : MonoBehaviour {
+/// <summary>
+/// An event triggered by game state changing.
+/// Arguments are the old state and new state, respectively.
+/// </summary>
+public class GameStateChange : UnityEvent<GameState, GameState> { };
 
-    public GameState gameState;
+public class TheManager : MonoBehaviour {
+    private static TheManager instance;
+    public static TheManager Instance { get { return instance; } }
+
+    /// <summary>
+    /// This section tracks the current overall game state,
+    /// its history, and notifies listeners about changes.
+    /// This is experimental, but if it's useful, we may factor
+    /// it out into its own generic class.
+    /// </summary>
+    #region Game State Management
+
+    /// <summary>
+    /// Current game state.
+    /// Still public so that it is exposed in the editor.
+    /// Don't set this from within code.
+    /// </summary>
+    public GameState _gameState = GameState.Playing;
+
+    private GameState oldState;
+
+    /// <summary>
+    /// A list of event listeners for state changes.
+    /// Use this if it simplifies your code, but if you really need to check each
+    /// Update, you can just keep a reference to TheManager.
+    /// </summary>
+    public GameStateChange stateChangeListeners;
+
+    public GameState gameState
+    {
+        get
+        {
+            return _gameState;
+        }
+        set
+        {
+            oldState = _gameState;
+            _gameState = value;
+            stateChangeListeners.Invoke(oldState, _gameState);
+        }
+    }
+
     public GameObject pauseMenu;
     private float oldTimeScale;
-
-    void Start () { }
-
-    public void Log(string msg)
-    {
-        Debug.Log(msg);
-    }
 
     public void CutsceneStart()
     {
@@ -29,6 +81,7 @@ public class TheManager : MonoBehaviour {
     public void Pause()
     {
         gameState = GameState.Paused;
+        // TODO: move this to a separate script?
         pauseMenu.SetActive(true);
         oldTimeScale = Time.timeScale;
         Time.timeScale = 0.0f;
@@ -36,18 +89,39 @@ public class TheManager : MonoBehaviour {
 
     public void Resume()
     {
-        gameState = GameState.Playing;
+        gameState = oldState;
+        // TODO: move this to a separate script?
         pauseMenu.SetActive(false);
         Time.timeScale = oldTimeScale;
     }
 
-    // Update is called once per frame
+    public void OpenInGameMenu()
+    {
+        // this just makes using Resume() easier
+        oldTimeScale = Time.timeScale;
+        gameState = GameState.InGameMenu;
+    }
+
+    #endregion
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown("Pawse"))
+        {
             switch (gameState)
             {
                 case GameState.Playing:
+                case GameState.InGameMenu:
+                case GameState.Cutscene:
                     Pause();
                     break;
                 case GameState.Paused:
@@ -56,5 +130,30 @@ public class TheManager : MonoBehaviour {
                 default:
                     break;
             }
+        }
+        else if (Input.GetKeyDown("Quick Menu"))
+        {
+            switch (gameState)
+            {
+                case GameState.Playing:
+                    OpenInGameMenu();
+                    break;
+                case GameState.InGameMenu:
+                    //TODO: make separate?
+                    Resume();
+                    break;
+                case GameState.Paused:
+                case GameState.Cutscene:
+                case GameState.Null:
+                default:
+                    // nada
+                    break;
+            }
+        }
+    }
+
+    public void Log(string msg)
+    {
+        Debug.Log(msg);
     }
 }
